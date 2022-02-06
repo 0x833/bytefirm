ba2de3a1fb7bf4d1514fb0a860861c8a5de5484b
Meyer <erick@bytefirm.xyz>
2022-02-05 23:17:10 -0800

    Update format

--- src/main.rs
+++ src/main.rs
@@ -75,7 +75,7 @@ impl App {
         html! {
             <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
                 <div class="navbar-brand">
-                    <h1 class="navbar-item is-size-3">{ "Yew Blog" }</h1>
+                    <h1 class="navbar-item is-size-3">{ "ByteFirm" }</h1>
 
                     <button class={classes!("navbar-burger", "burger", active_class)}
                         aria-label="menu" aria-expanded="false"
